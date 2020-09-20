355086821b0c96cbbf573360572c7846fcf709c0
Local Admin Carlo Camiletti <acarloc@eeghlan.net>
2020-09-20 13:54:29 +0200

    init

---
+++ src/main/java/nl/ccs/FunctionalInterfacesApplication.java
@@ -0,0 +1,13 @@
+package nl.ccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalInterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalInterfacesApplication.class, args);
+	}
+
+}
